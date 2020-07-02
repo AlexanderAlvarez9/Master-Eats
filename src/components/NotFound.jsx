5647179ac78d5c0e6c45ba570dd34cfda23c8b04
Walter <walter.salas@me.com>
2020-07-02 13:09:27 -0500

    NotFound and Success components

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../assets/styles/NotFound.scss'
+
+const notfound = () => {
+    return (
+    <div className="notfound">
+        <img src="" alt=""/>
+        <h1>Error</h1>
+    </div>
+    )
+}
+
+export default notfound
---
+++ src/components/Success.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/styles/Success.scss'
+
+const Success = () => {
+    return (
+    <div className="success" >
+        <h1>Success</h1>
+    </div>
+    )
+}
+
+export default Success
