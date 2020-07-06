12ab529464c7c089794121a5feb7d27b5dcbee05
Juan Camilo Moreno <juancamilomg21@gmail.com>
2020-07-05 19:42:50 -0500

    Conflict fixed in App.jsx

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import '../assets/styles/components/App.scss';
+import '../assets/styles/components/App.scss'
 
 import Containers from './Containers';
-import Layout from '../components/Layout';
+import Layout from '../components/Layout'
 import HelloWorld from '../components/HelloWorld';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
@@ -13,59 +13,61 @@ import NotFound from '../components/NotFound';
 function App() {
   const initialState = [
     {
-      id: 1,
-      image: '../assets/imgs/cerveza.png',
-      title: 'Carne de res',
-      price: 12000,
-      categorie: 'Carnes',
+      'id': 1,
+      'image': '../assets/imgs/cerveza.png',
+      'title': 'Carne de res',
+      'price': 12000,
+      'categorie': 'Carnes',
     },
     {
-      id: 2,
-      image: '../assets/imgs/img.png',
-      title: 'Zanahoria',
-      price: 1000,
-      categorie: 'Verdura',
+      'id': 2,
+      'image': '../assets/imgs/img.png',
+      'title': 'Zanahoria',
+      'price': 1000,
+      'categorie': 'Verdura',
     },
     {
-      id: 3,
-      image: '../assets/imgs/img.png',
-      title: 'Yogourt',
-      price: 3000,
-      categorie: 'lacteos',
+      'id': 3,
+      'image': '../assets/imgs/img.png',
+      'title': 'Yogourt',
+      'price': 3000,
+      'categorie': 'lacteos',
     },
     {
-      id: 4,
-      image: '../assets/imgs/img.png',
-      title: 'Atun',
-      price: 3500,
-      categorie: 'Enlatado',
+      'id': 4,
+      'image': '../assets/imgs/img.png',
+      'title': 'Atun',
+      'price': 3500,
+      'categorie': 'Enlatado',
     },
     {
-      id: 5,
-      image: '../assets/imgs/img.png',
-      title: 'Papas',
-      price: 1500,
-      categorie: 'Snacks',
+      'id': 5,
+      'image': '../assets/imgs/img.png',
+      'title': 'Papas',
+      'price': 1500,
+      'categorie': 'Snacks',
     },
   ];
 
   return (
     <React.Fragment>
       <Layout>
+
         <Containers>
-          {initialState.length > 0 && (
+          {initialState.length > 0 &&
             <Categories title="Categoria Dinamica">
               <Carousel>
-                {initialState.map((item) => (
+                {initialState.map(item =>
                   <Card key={item.id} {...item} />
-                ))}
+                )}
               </Carousel>
             </Categories>
-          )}
+          }
         </Containers>
+
       </Layout>
     </React.Fragment>
-  );
+  )
 }
 
 export default App;
