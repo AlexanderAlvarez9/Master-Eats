fc3991efbc58b002de9ed0c19e54104da95dc3c2
CamiloASuarez <camiloandressuarez1@gmail.com>
2020-07-16 12:57:00 -0500

    fixing some conflicts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-//import HelloWorld from "./components/HelloWorld";
+
 import UserAccount from "./components/UserAccount";
 
 ReactDOM.render(<UserAccount />, document.getElementById("app"));
