868ca17085d7ee6ddd43e4fc14df4dd7b9ed4d40
Alexander <jhonalexander9@gmail.com>
2020-07-27 20:02:13 -0500

    hot fix to user account

--- src/components/UserAccountPayment.jsx
+++ src/components/UserAccountPayment.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import '../assets/styles/components/UserAccount.scss';
 
-const UserAccountPayment =()=>{
-  return(
-    <h1>Payment Component</h1>
+const UserAccountPayment = () => {
+  return (
+    <div className="UserAccount__container__menu-settings">
+      <h2>Tus Metodos de Pago</h2>
+      <form action="">
+        <div className="form-wrapper">
+          <div className="form-row">
+            <label htmlFor="pname">Tarjeta de Credito #:</label><br />
+            <input type="text" id="calle" disabled="disable" placeholder="**** 4587" />
+          </div>
+          <div className="form-row">
+            <label htmlFor="lname">Nombre:</label><br />
+            <input type="text" id="barrio" disabled="disable" placeholder="Juan Carlos Gomez" />
+          </div>
+        </div>
+        <div className="form-button">
+          <button type="submit" form="" value="Submit">Modificar</button>
+        </div>
+      </form>
+    </div>
   )
 }
 
